ea0ffe4fb6731e9fb6fe5796c2af44973b3c6d63
Davi Carneiro <davicarneiro@ufrj.br>
2021-07-18 11:48:35 -0300

    Iniciando os trabalhos: dados pessoais, githubiando, instalando pacotes, lendo pacotes

--- scripts/analises.R
+++ scripts/analises.R
@@ -1,3 +1,31 @@
+#Script: analises
+fs::dir_tree() #vendo diretorio (pastas)
+
+#githubiando-----------------------------------------------------------------
+
 library(usethis)
 use_git()
 use_github()
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+
+#Trabalho Final | Introducao a programacao com R
+#Nome: Davi Carneiro Geraldo
+#e-mail: davicarneiro@ufrj.br
+#github: @davicarneirobr
+
+# Instalando os pacotes------------------------------------------------------
+
+install.packages("dplyr")
+install.packages("readr")
+
+# Lendo os pacotes-----------------------------------------------------------
+
+library(dplyr)
+library(readr)
+
+# Lendo arquivos ------------------------------------------------------------
+
+
+
